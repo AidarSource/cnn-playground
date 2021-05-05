a825eba420b972f3244d2f713af7b4ad23924c4f
Switchback <aidartemirgaliev@gmail.com>
2021-05-06 00:39:19 +0600

    upd

--- cnn.py
+++ cnn.py
@@ -123,7 +123,7 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
                 running_corrects += torch.sum(preds == labels.data)
 
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataloaders)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
