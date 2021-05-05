175b108d0cb45c812dc4853b8d63d17cb5d2b8b4
Switchback <aidartemirgaliev@gmail.com>
2021-05-05 23:17:59 +0600

    upload

--- cnn.py
+++ cnn.py
@@ -1,87 +1,168 @@
-# about torch...
-import torch
-import torch.nn as nn
-import torch
-import torch.optim as optim
-import torch.nn.functional as F
-
-from torchvision import datasets, models, transforms
-from torch.utils.data import DataLoader, Dataset
-
-# Using numpy
-import numpy as np
-
-# For data load or save
-import pandas as pd
-
-# Visualize some datasets
-import matplotlib.pyplot as plt
-
-# Check our work directory
+# Standard library
+import copy
+import glob
+import multiprocessing
 import os
-
-# To unzip datasets
+import time
 import zipfile
 
+# Pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models, transforms
 
-# Hyper parameters
-lr = 0.001 # learning_rate
-batch_size = 100 # we will use mini-batch method
-epochs = 10 # How much to train a model
-
-
-# Set seed and random value
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-torch.manual_seed(1234)
-if device =='cuda':
-    torch.cuda.manual_seed_all(1234)
-
-
-# Load train, test data
-os.listdir('input/dogs-vs-cats-redux-kernels-edition')
-
-os.makedirs('data', exist_ok=True)
-
-base_dir = 'input/dogs-vs-cats-redux-kernels-edition'
-train_dir = 'data/train'
-test_dir = 'data/test'
-
-with zipfile.ZipFile(os.path.join(base_dir, 'train.zip')) as train_zip:
-    train_zip.extractall('data')
-
-with zipfile.ZipFile(os.path.join(base_dir, 'test.zip')) as test_zip:
-    test_zip.extractall('data')
-
-
-import glob
-
-train_list = glob.glob(os.path.join(train_dir,'*.jpg'))
-test_list = glob.glob(os.path.join(test_dir, '*.jpg'))
-
+# Related third party
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from PIL import Image
+from skimage import io, transform
 from sklearn.model_selection import train_test_split
-train_list, val_list = train_test_split(train_list, test_size=0.2)
-
-# Data Augmentation
-train_transforms = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-])
-
-val_transforms = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-])
+from tqdm.notebook import tqdm
 
 
-test_transforms = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor()
-])
+# Google colab dataset load
+with zipfile.ZipFile('./drive/My Drive/Data Set/dogs-vs-cats-redux-kernels-edition.zip') as entire_zip:
+    entire_zip.extractall('.')
+
+with zipfile.ZipFile('./train.zip') as train_zip:
+    train_zip.extractall('.')
+
+with zipfile.ZipFile('./test.zip') as test_zip:
+    test_zip.extractall('.')
+
+train_dir = './train'
+test_dir = './test'
+
+# -------------------
+# Global declarations
+# -------------------
+input_size = 224
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+# Number of classes in the dataset
+num_classes = 2 # dog, cat
+
+# Batch size for training (change depending on how much memory you have)
+batch_size = 32
+
+# Number of epochs to train for
+num_epochs = 2
+
+# Flag for feature extracting. When False, we finetune the whole model,
+# when True we only update the reshaped layer params
+feature_extract = True
+
+# Switch to perform multi-process data loading
+num_workers = multiprocessing.cpu_count()
+
+# -------------------
+
+# Helper Functions
+
+# train data file looks './train/dog.10435.jpg'
+# test data file looks './test/10435.jpg'
+def extract_class_from(path):
+    file = path.split('/')[-1]
+    return file.split('.')[0]
+
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
+    since = time.time()
+
+    history = {
+        'accuracy': [],
+        'val_accuracy': [],
+        'loss': [],
+        'val_loss': []
+    }
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train() # Set model to training mode
+            else:
+                model.eval() # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data
+            for inputs, labels in tqdm(dataloaders[phase]):
+                inputs = inputs.to('gpu')
+                labels = labels.to('gpu')
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataloaders)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+            if phase == 'train':
+                history['accuracy'].append(epoch_acc.item())
+                history['loss'].append(epoch_loss)
+            else:
+                history['val_accuracy'].append(epoch_acc.item())
+                history['val_loss'].append(epoch_loss)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, history
+
+
+# Load data
+all_train_files = glob.glob(os.path.join(train_dir, '*.jpg'))
+train_list, val_list = train_test_split(all_train_files, random_state=42)
+
+print(len(train_list))
+print(len(val_list))
+
+
+
+
+
+
+
+
+
 
